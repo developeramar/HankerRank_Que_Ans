56b6b3748a28ee45aa5e3f5cbb71ed2a1ffb5f9d
Amar Kumar Chaudhari <chaudhariamarkumar662@gmail.com>
2023-08-26 22:36:37 +0530

    Hanker Rank Que Ans

---
+++ HankerRankQue_4.java
@@ -0,0 +1,53 @@
+/*Objective
+In this challenge, we're going to use loops to help us do some simple math.
+
+Task
+Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: N x i = result.
+
+Input Format
+
+A single integer, .
+
+Constraints
+
+Output Format
+
+Print  lines of output; each line  (where ) contains the  of  in the form:
+N x i = result.
+
+Sample Input
+
+2
+Sample Output
+
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+ */
+
+import java.util.Scanner;
+
+public class HankerRankQue_4 {
+    public static void main(String[] args) {
+        
+        int N;
+        System.out.println("Enter any nymber --");
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        for(int i=1 ; i<=10 ; i++){
+            int result = N*i;
+            System.out.printf("%d x %d = %d\n" , N , i , result);
+        }
+
+
+    }
+}
